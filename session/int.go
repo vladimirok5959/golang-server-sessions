bf404520401d0cd2d7cacbb7637203f661dba890
Volodymyr Tkach <vladimirok5959@gmail.com>
2022-06-28 01:00:28 +0300

    Don't use this

--- session/int.go
+++ session/int.go
@@ -1,25 +1,25 @@
 package session
 
-func (this *Session) IsSetInt(name string) bool {
-	if _, ok := this.v.Int[name]; ok {
+func (s *Session) IsSetInt(name string) bool {
+	if _, ok := s.v.Int[name]; ok {
 		return true
 	} else {
 		return false
 	}
 }
 
-func (this *Session) GetInt(name string, def int) int {
-	if v, ok := this.v.Int[name]; ok {
+func (s *Session) GetInt(name string, def int) int {
+	if v, ok := s.v.Int[name]; ok {
 		return v
 	} else {
 		return def
 	}
 }
 
-func (this *Session) SetInt(name string, value int) {
-	isset := this.IsSetInt(name)
-	this.v.Int[name] = value
+func (s *Session) SetInt(name string, value int) {
+	isset := s.IsSetInt(name)
+	s.v.Int[name] = value
 	if isset || value != 0 {
-		this.c = true
+		s.c = true
 	}
 }
--- session/string.go
+++ session/string.go
@@ -1,25 +1,25 @@
 package session
 
-func (this *Session) IsSetString(name string) bool {
-	if _, ok := this.v.String[name]; ok {
+func (s *Session) IsSetString(name string) bool {
+	if _, ok := s.v.String[name]; ok {
 		return true
 	} else {
 		return false
 	}
 }
 
-func (this *Session) GetString(name string, def string) string {
-	if v, ok := this.v.String[name]; ok {
+func (s *Session) GetString(name string, def string) string {
+	if v, ok := s.v.String[name]; ok {
 		return v
 	} else {
 		return def
 	}
 }
 
-func (this *Session) SetString(name string, value string) {
-	isset := this.IsSetString(name)
-	this.v.String[name] = value
+func (s *Session) SetString(name string, value string) {
+	isset := s.IsSetString(name)
+	s.v.String[name] = value
 	if isset || value != "" {
-		this.c = true
+		s.c = true
 	}
 }
